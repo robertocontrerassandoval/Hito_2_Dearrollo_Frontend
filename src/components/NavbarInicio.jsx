784f96bf432b477240d8f26fa78002b7a57b1733
Roberto Contreras <contreras_sandoval@hotmail.com>
2024-08-28 14:09:11 -0400

    :tada:hito-2-frontend 2808-2

---
+++ src/components/NavbarInicio.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo from '../img/logo.jpeg';
+
+function NavbarInicio() {
+  return (
+    <Navbar className="navbar-inicio" bg="white" variant="light" expand="lg">
+    <Container>
+
+        <Container style={{ maxWidth: '200px', width: '100%' }}>
+        <Navbar.Brand className="ml-auto">
+        <img
+          src={logo}
+          alt="Logo"
+          className="d-inline-block align-top"
+          style={{ height: '50px' }}
+         />
+             </Navbar.Brand>
+        </Container>
+         
+    
+        <Container>
+        <Nav defaultActiveKey="/inicio" as="ul">
+     
+      <Nav.Item as="li">
+        <Nav.Link eventKey="inicio" t>Inicio</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li" >
+        <Nav.Link href="productos">Productos</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link href="favoritos">Favoritos</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link href="perfil">Mi Perfil</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  
+
+        </Container>
+
+
+    
+      <Navbar.Brand className="mr-auto">
+        <Link to="/agragar" className="btn btn-primary mr-2">Agregar Productos</Link>
+      </Navbar.Brand>
+
+      <Navbar.Brand className="mr-auto">
+        <Link to="/home" className="btn btn-danger mr-2 " >Salir</Link>
+      </Navbar.Brand>
+
+      
+    </Container>
+  </Navbar>
+  )
+}
+
+export default NavbarInicio
--- src/view/Inicio.jsx
+++ src/view/Inicio.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import NavbarInicio from '../components/NavbarInicio'
+import { Container,Row, Col, Form, Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Inicio() {
+
+const Inicio = () => {
   return (
-    <div>Inicio</div>
+
+   <>
+   <Container>
+     
+     <Container className="container-login d-flex flex-column justify-content-space-around">
+     <Container >
+          <NavbarInicio/>
+          </Container>
+           pagina inicio
+          </Container>    
+            </Container>
+       
+   </>
+
+    
+  
   )
 }
 
