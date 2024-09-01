f1607222e70d9fbe685d804c815d718c7971f7e5
Ruben Norambuena <rubenalejhandro.norambuena@gmail.com>
2024-09-01 18:12:06 -0400

    Version-7

--- src/view/Perfil.jsx
+++ src/view/Perfil.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import NavbarInicio from '../components/NavbarInicio'
-import { Container,Row, Col, Form, Button} from 'react-bootstrap'
+
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import NavbarInicio from '../components/NavbarInicio';
+import { Container, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useAppContext } from '../context/AppContext';
 
 const Perfil = () => {
+  const { user, logoutUser } = useAppContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
+  if (!user) {
+    return null; // Evita el renderizado mientras se redirige
+  }
+
   return (
-    
-   <>
-   <Container className="container-inicio d-flex flex-column justify-content-space-around">
-     
-     
-     <Container className='container-navbar-inicio'>
-          <NavbarInicio/>
+    <>
+      <Container className="container-inicio d-flex flex-column justify-content-space-around">
+        <Container className='container-navbar-inicio'>
+          <NavbarInicio />
+        </Container>
+
+        <Container>
+          <h1 className="text-center">Perfil de Usuario</h1>
+          <div>
+            <h2>{user.nombre}</h2>
+            <p>Email: {user.email}</p>
+            <Button variant="danger" onClick={logoutUser}>Cerrar Sesión</Button>
+          </div>
+        </Container>
       </Container>
-
-      
-
-          <Container >
-           pagina perfil
-          </Container>   
-
-            </Container>
-       
-   </>
-  )
+    </>
+  );
 }
 
-export default Perfil
+export default Perfil;
+
